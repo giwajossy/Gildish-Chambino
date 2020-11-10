e62c00d37e5182429545a0fe92efe4e69a635a7c
giwajossy <giwajosef@gmail.com>
2020-11-10 04:10:43 +0100

    Refactor Arena Behavior

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,18 @@
+
+function myFunction() {
+    location.reload();
+}
+
 function playButton() {
+    
+    setTimeout(myFunction, 2000);
+    
 
     var dice = [1, 2, 3, 4, 5, 6];
     var lengthOfDice = dice.length;
     var randomValueForPlayerOne = dice[Math.floor(Math.random() * lengthOfDice)];
     var randomValueForPlayerTwo = dice[Math.floor(Math.random() * lengthOfDice)];
-    // console.log(randomValue);
+    
     
     document.querySelector(".left h3 span").textContent = randomValueForPlayerOne;
     document.querySelector(".right h3 span").textContent = randomValueForPlayerTwo;
@@ -14,13 +22,16 @@ function playButton() {
     if (randomValueForPlayerOne > randomValueForPlayerTwo) {
         document.getElementById("status").textContent = "Player one Wins";
         document.getElementById("author").textContent = "Congratulation! Play again";
+        document.getElementsByClassName("left")[0].style.border = ".3em solid rgba(192,57,43,1)";
     } else if (randomValueForPlayerTwo > randomValueForPlayerOne) {
         document.getElementById("status").textContent = "Player two Wins";
         document.getElementById("author").textContent = "Congratulation! Play again";
+        document.getElementsByClassName("right")[0].style.border = ".3em solid rgba(192,57,43,1)";
     } else {
         document.getElementById("status").textContent = "Tie Game";
         document.getElementById("author").textContent = "Play again";
     }
+    
     
 
     // var playerOne = document.querySelector(".left img").setAttribute("src");
